07c8445f7849f9684e881aa0b4c597b3ea4923e9
conor-behard333 <conorbehard@hotmail.co.uk>
2019-03-29 23:27:13 +0000

    added progress bars

--- src/UserInterfaces/PredictionUI.java
+++ src/UserInterfaces/PredictionUI.java
@@ -7,10 +7,11 @@ import java.text.DecimalFormat;
 public class PredictionUI extends JFrame {
     private String[] confidence = new String[10];
     private JLabel[] predictionsLabel = new JLabel[10];
+    private JProgressBar[] progressBars = new JProgressBar[10];
 
     public PredictionUI() {
         setTitle("Neural Network - Digit Classifier - Predictions");
-        setSize(200, 600);
+        setSize(500, 600);
         setBackground(Color.black);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(1410, 220);
@@ -22,30 +23,63 @@ public class PredictionUI extends JFrame {
     private void createUI() {
         int yPos = 10;
         for (int i = 0; i < 10; i++) {
-            predictionsLabel[i] = new JLabel("Guess " + i + ": 0.00%");
-            predictionsLabel[i].setSize(200, 100);
-            predictionsLabel[i].setFont(new Font(getName(), Font.PLAIN, 20));
-            predictionsLabel[i].setLocation(10, yPos);
+            predictionsLabel[i] = createLabel(yPos, i);
+            progressBars[i] = createProgressBar(yPos);
             yPos += 50;
             getContentPane().add(predictionsLabel[i]);
+            getContentPane().add(progressBars[i]);
         }
-        getContentPane().add(createPanel(500, 600));
+        getContentPane().add(createPanel());
     }
 
-    private JPanel createPanel(int x, int y) {
+    private JProgressBar createProgressBar(int yPos) {
+        JProgressBar temp = new JProgressBar();
+        temp.setSize(200, 20);
+        temp.setLocation(140, yPos);
+        temp.setMaximum(0);
+        temp.setMaximum(100);
+        temp.setStringPainted(true);
+        return temp;
+    }
+
+    private JLabel createLabel(int yPos, int i) {
+        JLabel temp = new JLabel(i + ": 0.00%");
+        temp.setSize(100, 20);
+        temp.setFont(new Font(getName(), Font.PLAIN, 20));
+        temp.setLocation(10, yPos);
+        return temp;
+    }
+
+    private JPanel createPanel() {
         JPanel temp = new JPanel();
-        temp.setLocation(x, y);
+        temp.setLocation(500, 600);
         return temp;
     }
 
     public void setPredictions(double[] outputs) {
-        DecimalFormat df = new DecimalFormat("#.##%");
+        DecimalFormat df = new DecimalFormat("#.##");
         for (int i = 0; i < 10; i++) {
-            confidence[i] = df.format(outputs[i]);
+            confidence[i] = df.format(outputs[i] * 100);
         }
-        String[] guess = {"Guess 0: " + confidence[0], "Guess 1: " + confidence[1], "Guess 2: " + confidence[2], "Guess 3: " + confidence[3], "Guess 4: " + confidence[4], "Guess 5: " + confidence[5], "Guess 6: " + confidence[6], "Guess 7: " + confidence[7], "Guess 8: " + confidence[8], "Guess 9: " + confidence[9]};
+        String[] guess = {"0: " + confidence[0] + "%", "1: " + confidence[1] + "%", "2: " + confidence[2] + "%", "3: " + confidence[3] + "%", "4: " + confidence[4] + "%", "5: " + confidence[5] + "%", "6: " + confidence[6] + "%", "7: " + confidence[7] + "%", "8: " + confidence[8] + "%", "9: " + confidence[9] + "%"};
         for (int i = 0; i < 10; i++) {
+            int value = (int) (Double.parseDouble(confidence[i]));
+            progressBars[i].setForeground(getPbColour(value));
+            progressBars[i].setValue(value);
             predictionsLabel[i].setText(guess[i]);
         }
+    }
+
+    private Color getPbColour(int value) {
+        if (value >= 0 && value < 20) {
+            return Color.red;
+        } else if (value >= 20 && value < 40) {
+            return Color.pink;
+        } else if (value >= 40 && value < 80) {
+            return Color.yellow;
+        } else if (value >= 80 && value < 100) {
+            return Color.green;
+        }
+        return null;
     }
 }
