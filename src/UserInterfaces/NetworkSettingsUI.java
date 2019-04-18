07516bdaab046af582702129cf72f26b5929360b
conor-behard333 <conorbehard@hotmail.co.uk>
2019-04-18 23:01:40 +0100

    Revert "Delete NetworkSettingsUI.java"
    This reverts commit fef0a57304e1e93460878c3fcc86d00a7ad68fb6.

---
+++ src/UserInterfaces/NetworkSettingsUI.java
@@ -0,0 +1,40 @@
+package UserInterfaces;
+
+import javax.swing.*;
+
+public class NetworkSettingsUI {
+    private int epochs;
+    private int batchSize;
+
+    public NetworkSettingsUI() {
+        JFrame frame = new JFrame();
+        String[] epochValues = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};//available epoch values
+        String[] batchSizeValues = {"10000", "20000", "30000", "40000", "50000", "60000"};//available batch size values 60000 is the max
+
+        //asks the user how many epochs they want the network to do
+        String epochs = (String) JOptionPane.showInputDialog(frame, "How many epochs do you want?", "Neural Network Settings", JOptionPane.QUESTION_MESSAGE, null, epochValues, epochValues[0]);
+
+        if ((epochs != null) && (epochs.length() > 0)) {
+            this.epochs = Integer.parseInt(epochs);
+        } else {
+            System.exit(0);
+        }
+
+        //the size of the training data used
+        String batchSize = (String) JOptionPane.showInputDialog(frame, "How many training value do you want to use?", "Neural Network Settings", JOptionPane.QUESTION_MESSAGE, null, batchSizeValues, batchSizeValues[0]);
+
+        if ((batchSize != null) && (batchSize.length() > 0)) {
+            this.batchSize = Integer.parseInt(batchSize);
+        } else {
+            System.exit(0);
+        }
+    }
+
+    public int getEpochs() {
+        return epochs;
+    }
+
+    public int getBatchSize() {
+        return batchSize;
+    }
+}
