2d52b2ea832f2c05f76e18b1d73a966df80472f9
conor-behard333 <conorbehard@hotmail.co.uk>
2019-04-18 23:01:38 +0100

    Revert "Delete LoadingUI.java"
    This reverts commit a6671a67ba747a3f26870d4d048add8857b3f1cc.

---
+++ src/UserInterfaces/LoadingUI.java
@@ -0,0 +1,29 @@
+package UserInterfaces;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoadingUI extends JFrame {
+    public LoadingUI() {
+        setTitle("Loading - Please Wait");
+        setSize(300, 50);
+        setBackground(Color.black);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        addLoadingBar();
+    }
+
+    private void addLoadingBar() {
+        JPanel panel = new JPanel();
+        panel.setSize(getWidth(), getHeight());
+
+        JProgressBar load = new JProgressBar();
+        load.setSize(getWidth(), getHeight());
+        load.setLocation(0, 0);
+        load.setIndeterminate(true);
+
+        getContentPane().add(load);
+        getContentPane().add(panel);
+    }
+}
